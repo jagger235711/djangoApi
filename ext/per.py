6e4dd6ab6723bd9cfd020e02760b9fa0f050ba44
巫文杰 <252587809@qq.com>
2023-11-30 22:06:20 +0800

    权限-源码流程

--- ext/per.py
+++ ext/per.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import BasePermission
 
 
 class MyPermission(BasePermission):
+    message = {"status": False, "msg": "无权访问"}
+
     def has_permission(self, request, view):
         v1 = random.randint(1, 3)
         if v1 == 2:
