83882d895a185b990b68d61f5a0b5ae00dc42e05
巫文杰 <252587809@qq.com>
2024-01-21 16:47:44 +0800

    注册

--- api/views.py
+++ api/views.py
@@ -134,3 +134,46 @@ class CommentView(APIView):
         ser = CommentSerializer(instance=querySet, many=True)
         context = {"code": 200, "msg": "ok", "data": ser.data}
         return Response(context)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # user = serializers.CharField(source="user.username")
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = models.UserInfo
+        # 字段执行顺序与fields定义顺序一致
+        fields = ["id", "username", "password", "confirm_password"]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "password": {"write_only": True},
+        }
+
+    def validate_confirm_password(self, value):
+        password = self.initial_data.get("password")
+        if password != value:
+            raise serializers.ValidationError("密码不一致")
+        return value
+
+
+class RegisterView(APIView):
+    # def get(self, request, *args, **kwargs):
+    #     """获取博客详情"""
+    #     pk = kwargs.get("pk")
+    #     instance = models.Blog.objects.filter(id=pk).first()
+    #     if not instance:
+    #         return Response({"code": 400, "msg": "not found"})
+    #     ser = BlogDetailSerializer(instance=instance, many=False)
+    #     context = {"code": 200, "msg": "ok", "data": ser.data}
+    #     return Response(context)
+    def post(self, request, *args, **kwargs):
+        """注册"""
+        ser = RegisterSerializer(data=request.data)
+        if not ser.is_valid():
+            content = {"code": 400, "msg": "注册失败", "data": ser.errors}
+        else:
+            # 排除不到保存的字段
+            ser.validated_data.pop("confirm_password")
+            ser.save()
+            content = {"code": 200, "msg": "ok", "data": ser.data}
+        return Response(content)
--- blog/urls.py
+++ blog/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("api/blog/", views.BlogView.as_view()),
     path("api/blog/<int:pk>/", views.BlogDetailView.as_view()),
     # path("api/comment/", views.BlogDetailView.as_view()),#?blog_id=1
-    path("api/comment/<int:blog_id>/",  views.CommentView.as_view()),
+    path("api/comment/<int:blog_id>/", views.CommentView.as_view()),
+    path("api/register/", views.RegisterView.as_view()),
 ]
